9fdb2e0956caef81fee45897ea58ad687b643094
juniehyun <juniehyun@gmail.com>
2016-10-14 11:43:03 -0700

    js ajax - Junie Hyun

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,7 +1,7 @@
 //Create GLOBAL variable below here on line 2
+var global_result;
 
-
-$(document).ready(function(){
+    $(document).ready(function(){
     $('button').click(function(){
         console.log('click initiated');
         $.ajax({
@@ -9,9 +9,25 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
+                global_result = result;
+                var movie = result.feed.entry[0]['im:image'][2].label;
+                console.log(movie);
+                for(i=0; i<result.feed.entry.length;i++){
+                    var images = result.feed.entry[i]['im:image'][2].label;
+                    var img = $('<img>',{
+                        src: images
+                        });
+                    $('#main').append(img);
+                    var movie_title = result.feed.entry[i]['title'].label;
+                    var movie_title1 = $('<h1>',{
+                        text: movie_title,
+                    });
+                    $(movie_title1).appendTo('#main');
+                }
 
             }
         });
         console.log('End of click function');
     });
 });
+
